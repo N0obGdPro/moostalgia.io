06c3bd3887f81877d4dae6a502ceb855589dab4d
NoobGdPro <141958415+N0obGdPro@users.noreply.github.com>
2026-02-16 23:47:26 +0100

    Change default start item ID from 4 to 5

--- shared/config.js
+++ shared/config.js
@@ -61,7 +61,7 @@ var weaponVariants = [{
 }];
 
 // Player spawn defaults
-var defaultStartItems = [0, 3, 6, 10, 4];  // Item IDs players spawn with
+var defaultStartItems = [0, 3, 6, 10, 5];  // Item IDs players spawn with
 var defaultStartWeapons = [0];  // Weapon IDs players spawn with
 var startResources = {
     normal: 0,    // Starting score/points
